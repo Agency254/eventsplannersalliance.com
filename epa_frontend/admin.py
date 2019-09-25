590ea6c87c204df410e67ae5d113f94f604f13b8
Newton Karanu <karanunewton4@gmail.com>
2019-09-25 09:45:26 +0300

    linked models to django admin

--- epa_frontend/admin.py
+++ epa_frontend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from epa_frontend.models import Merchants, Properties, Profile, Orders, OrderItems
+
+admin.site.register(Merchants)
+admin.site.register(Properties)
+admin.site.register(Profile)
+admin.site.register(Orders)
+admin.site.register(OrderItems)
