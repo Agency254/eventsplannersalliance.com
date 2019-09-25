e392f1a1b40513084e06764c1f0dcc50b8bd6608
Newton Karanu <karanunewton4@gmail.com>
2019-09-25 20:18:25 +0300

    reset django suffix for forein keys and run migrations

--- epa_frontend/models.py
+++ epa_frontend/models.py
@@ -29,9 +29,16 @@ def save_user_profile(sender, instance, **kwargs):
 class Merchants(models.Model):
     id = models.AutoField(primary_key=True)
     merchant_name = models.CharField(max_length=100)
+    merchant_description = models.CharField(max_length=200, default="input merchant description")
     country_code = CountryField(blank_label='(select country)')
-    created_at = models.DateTimeField()
-    admin_id = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="user_profile")
+    created_at = models.DateTimeField(default=datetime.now)
+    admin_id = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="user_profile",
+        db_column='admin_id',
+        default=1
+    )
 
 
 class PropertyType(models.Model):
@@ -39,16 +46,30 @@ class PropertyType(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=50)
     images = models.ImageField(upload_to='property_type/')
-    merchant_id = models.ForeignKey(Merchants, on_delete=models.CASCADE, related_name="merchant_information")
+    merchant_id = models.ForeignKey(
+        Merchants,
+        on_delete=models.CASCADE,
+        related_name="merchant_information",
+        db_column='merchant_id'
+    )
 
 
 class Properties(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     images = models.ImageField(upload_to='properties/')
-    merchant_id = models.ForeignKey(Merchants, on_delete=models.CASCADE, related_name="merchant_details")
-    property_type_id = models.ForeignKey(PropertyType, on_delete=models.CASCADE,
-                                         related_name="property_type_information")
+    merchant_id = models.ForeignKey(
+        Merchants,
+        on_delete=models.CASCADE,
+        related_name="merchant_details",
+        db_column='merchant_id'
+    )
+    property_type_id = models.ForeignKey(
+        PropertyType,
+        on_delete=models.CASCADE,
+        related_name="property_type_information",
+        db_column='property_type_id'
+    )
     price = MoneyField(max_digits=14, decimal_places=2, default_currency='KES')
     status = models.CharField(max_length=50, choices=[
         ("booked", "booked"),
@@ -60,7 +81,12 @@ class Properties(models.Model):
 
 class Orders(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="user_information")
+    user_id = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="user_information",
+        db_column='user_id'
+    )
     status = models.CharField(max_length=50, choices=[
         ("paid", "paid"),
         ("unpaid", "unpaid")
@@ -69,7 +95,12 @@ class Orders(models.Model):
 
 
 class OrderItems(models.Model):
-    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name="order_details")
-    property_id = models.ForeignKey(Properties, on_delete=models.CASCADE, related_name="property_details")
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name="order_details")
+    property_id = models.ForeignKey(
+        Properties,
+        on_delete=models.CASCADE,
+        related_name="property_details",
+        db_column='property_id'
+    )
     start_of_stay = models.DateTimeField(default=datetime.now)
     end_of_stay = models.DateTimeField(default=datetime.now)
