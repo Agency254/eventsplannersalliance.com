f97cdc96b4245d042c26eb3a48b01e2df5f283af
Newton Karanu <karanunewton4@gmail.com>
2019-10-04 17:04:53 +0300

    refactored merchants views

--- epa_frontend/views/authentication.py
+++ epa_frontend/views/authentication.py
@@ -1,16 +1,16 @@
-from datetime import datetime
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 from django.shortcuts import render, redirect
 
-from epa_frontend.forms.authentication_forms import UserForm, ProfileForm, MerchantForm
-from epa_frontend.models import Merchants, Properties
+from epa_frontend.forms.authentication_forms import UserForm, ProfileForm
+from epa_frontend.models import EventsType
 
 
 def signup(request):
+    events_types = EventsType.objects.all()
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -22,12 +22,16 @@ def signup(request):
             return redirect('index')
     else:
         form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'user_creation_form': form})
+    return render(request, 'registration/signup.html', {
+        'user_creation_form': form,
+        "events_types": events_types,
+    })
 
 
 @login_required
 @transaction.atomic
 def update_profile(request):
+    events_types = EventsType.objects.all()
     print(request.user.profile)
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
@@ -44,87 +48,18 @@ def update_profile(request):
         profile_form = ProfileForm(instance=request.user.profile)
     return render(request, 'registration/update_profile.html', {
         'user_form': user_form,
+        "events_types": events_types,
         'profile_form': profile_form
     })
 
 
+@login_required
 def view_profile(request):
     current_user = request.user
     current_user_profile = current_user.profile
+    events_types = EventsType.objects.all()
     return render(request, 'registration/profile.html', {
         "user": current_user,
+        "events_types": events_types,
         "profile": current_user_profile
-    })
-
-
-@login_required
-def create_merchant(request):
-    current_user = request.user
-    current_user_merchants = Merchants.objects.filter(admin_id=current_user.profile.id)
-    if request.method == 'POST':
-        merchant_form = MerchantForm(request.POST)
-
-        if merchant_form.is_valid():
-            merchant_form.user_profile_id = current_user.id
-            merchant_form.created_at = datetime.now
-            merchant_form.save()
-            messages.success(request, 'The new merchant has been created succesfully')
-            return redirect('properties')
-        else:
-            messages.error(request, 'please correct the mistakes below')
-    else:
-        merchant_form = MerchantForm(request.POST)
-    return render(request, 'merchants/new_merchant.html', {
-        "merchant_form": merchant_form,
-        "current_merchants": current_user_merchants
-    })
-
-
-@login_required
-def update_merchant(request, pk):
-    current_user = request.user
-    current_user_merchants = Merchants.objects.filter(admin_id=current_user.profile.id, id=pk)
-    current_merchant = [e for e in current_user_merchants]
-    # print(current_merchant[0].merchant_name)
-    if request.method == 'POST':
-        merchant_form = MerchantForm(request.POST, instance=current_merchant[0])
-        if merchant_form.is_valid():
-            merchant_form.save()
-            messages.success(request, 'Your merchant was successfully updated!')
-            return redirect('view_merchant', pk=current_merchant[0].id)
-        else:
-            messages.error(request, 'Please fix the errors below!')
-    else:
-        merchant_form = MerchantForm(instance=current_merchant[0])
-    return render(request, 'merchants/update_merchant.html', {
-        "merchant_form": merchant_form,
-        "current_merchant": current_merchant[0]
-    })
-
-
-def view_merchant(request, pk):
-    current_user = request.user
-    user_creation_form = UserCreationForm()
-    user_authentication_form = AuthenticationForm()
-    merchant = Merchants.objects.get(pk=pk)
-    properties = Properties.objects.filter(merchant_id=merchant.id)
-    return render(request, 'merchants/merchant.html', {
-        "merchant": merchant,
-        "properties": properties,
-        "user_authentication_form": user_authentication_form,
-        "user_creation_form": user_creation_form,
-        "current_user": current_user
-    })
-
-
-def view_merchants(request):
-    current_user = request.user
-    user_creation_form = UserCreationForm()
-    user_authentication_form = AuthenticationForm()
-    merchants = Merchants.objects.all()
-    return render(request, 'merchants/merchants.html', {
-        "merchants": merchants,
-        "user_authentication_form": user_authentication_form,
-        "user_creation_form": user_creation_form,
-        "current_user": current_user
     })
---
+++ epa_frontend/views/merchants.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import redirect, render
+
+from epa_frontend.forms.authentication_forms import MerchantForm
+from epa_frontend.models import Merchants, EventsType, Events
+
+
+@login_required
+def create_merchant(request):
+    current_user = request.user
+    current_user_merchants = Merchants.objects.filter(admin_id=current_user.profile.id)
+    events_types = EventsType.objects.all()
+    if request.method == 'POST':
+        merchant_form = MerchantForm(request.POST)
+
+        if merchant_form.is_valid():
+            merchant_form.user_profile_id = current_user.id
+            merchant_form.created_at = datetime.now
+            merchant_form.save()
+            messages.success(request, 'The new merchant has been created succesfully')
+            return redirect('properties')
+        else:
+            messages.error(request, 'please correct the mistakes below')
+    else:
+        merchant_form = MerchantForm(request.POST)
+    return render(request, 'merchants/new_merchant.html', {
+        "merchant_form": merchant_form,
+        "events_types": events_types,
+        "current_merchants": current_user_merchants
+    })
+
+
+@login_required
+def update_merchant(request, pk):
+    current_user = request.user
+    current_user_merchants = Merchants.objects.filter(admin_id=current_user.profile.id, id=pk)
+    current_merchant = [e for e in current_user_merchants]
+    events_types = EventsType.objects.all()
+    # print(current_merchant[0].merchant_name)
+    if request.method == 'POST':
+        merchant_form = MerchantForm(request.POST, instance=current_merchant[0])
+        if merchant_form.is_valid():
+            merchant_form.save()
+            messages.success(request, 'Your merchant was successfully updated!')
+            return redirect('view_merchant', pk=current_merchant[0].id)
+        else:
+            messages.error(request, 'Please fix the errors below!')
+    else:
+        merchant_form = MerchantForm(instance=current_merchant[0])
+    return render(request, 'merchants/update_merchant.html', {
+        "merchant_form": merchant_form,
+        "events_types": events_types,
+        "current_merchant": current_merchant[0]
+    })
+
+
+def view_merchant(request, pk):
+    current_user = request.user
+    user_creation_form = UserCreationForm()
+    user_authentication_form = AuthenticationForm()
+    merchant = Merchants.objects.get(pk=pk)
+    events = Events.objects.filter(merchant_id=merchant.id)
+    events_types = EventsType.objects.all()
+    return render(request, 'merchants/merchant.html', {
+        "merchant": merchant,
+        "events": events,
+        "user_authentication_form": user_authentication_form,
+        "user_creation_form": user_creation_form,
+        "events_types": events_types,
+        "current_user": current_user
+    })
+
+
+def view_merchants(request):
+    current_user = request.user
+    user_creation_form = UserCreationForm()
+    user_authentication_form = AuthenticationForm()
+    merchants = Merchants.objects.all()
+    events_types = EventsType.objects.all()
+    return render(request, 'merchants/merchants.html', {
+        "merchants": merchants,
+        "user_authentication_form": user_authentication_form,
+        "user_creation_form": user_creation_form,
+        "events_types": events_types,
+        "current_user": current_user
+    })
