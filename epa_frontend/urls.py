93e02ce879a2a2db5676863c7f4f527553f0c651
Newton Karanu <karanunewton4@gmail.com>
2019-10-04 17:10:49 +0300

    refactored app routing --WIP

--- epa_frontend/urls.py
+++ epa_frontend/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.urls import path, include
 
-from epa_frontend.views import views, authentication
+from epa_frontend.views import views, authentication, merchants, events, events_type
+
 urlpatterns = [
     url('^$', views.home, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
@@ -9,12 +10,15 @@ urlpatterns = [
     path('accounts/profile/update/', authentication.update_profile, name='update_profile'),
     path('accounts/signup/', authentication.signup, name='signup'),
     path('accounts/login/', authentication.login, name='login'),
-    path('merchants/new/', authentication.create_merchant, name='create_merchant'),
-    path('merchants/', authentication.view_merchants, name='view_merchants'),
-    path('merchant/<int:pk>/', authentication.view_merchant, name='view_merchant'),
-    path('merchant/<int:pk>/update/', authentication.update_merchant, name='update_merchant'),
-    path('events/', views.view_events, name='view_events'),
+    path('merchants/new/', merchants.create_merchant, name='create_merchant'),
+    path('merchants/', merchants.view_merchants, name='view_merchants'),
+    path('merchant/<int:pk>/', merchants.view_merchant, name='view_merchant'),
+    path('merchant/<int:pk>/update/', merchants.update_merchant, name='update_merchant'),
+    path('events/', events.view_events, name='view_events'),
+    path('events/new/', events.create_event, name='create_events'),
     path('about/', views.view_about, name='view_about'),
-    path('event/<int:pk>/', views.view_event, name='view_event'),
-    path('event-type/<int:pk>/', views.view_event_type, name='view_event_type'),
+    path('event/<int:pk>/', events.view_event, name='view_event'),
+    path('event/<int:pk>/dashboard/', events.view_event_dashboard, name='view_event_dashboard'),
+    path('event-type/<int:pk>/', events_type.view_event_type, name='view_event_type'),
+    path('event-type/new/', events_type.create_event_type, name='create_event_type'),
 ]
