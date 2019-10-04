0a68a49071769b9665a61c944e9e12aacd0ba779
Newton Karanu <karanunewton4@gmail.com>
2019-10-04 17:09:51 +0300

    refactored events types

--- epa_frontend/admin.py
+++ epa_frontend/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from epa_frontend.models import Merchants, Properties, Profile, Orders, OrderItems, PropertyType
+from epa_frontend.models import Merchants, Events, Profile, Orders, OrderItems, EventsType
 
 admin.site.register(Merchants)
-admin.site.register(Properties)
+admin.site.register(Events)
 admin.site.register(Profile)
 admin.site.register(Orders)
 admin.site.register(OrderItems)
-admin.site.register(PropertyType)
+admin.site.register(EventsType)
---
+++ epa_frontend/views/events_type.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render
+
+from epa_frontend.models import Merchants, EventsType, Events
+
+
+@login_required
+def create_event_type(request):
+    current_user = request.user
+    merchant = Merchants.objects.filter(admin_id=current_user.id)
+    events_types = EventsType.objects.all()
+    return render(request, 'events_types/new_event_type.html', {
+        "events_types": events_types,
+    })
+
+
+@login_required
+def update_event_type(request, pk):
+    current_user = request.user
+    merchant = Merchants.objects.filter(admin_id=current_user.id)
+    user_form = UserCreationForm()
+    user_authentication_form = AuthenticationForm()
+    events_types = EventsType.objects.all()
+    return render(request, 'events_types/events_type.html', {
+        "event_type": events_types,
+        "user_creation_form": user_form,
+        "user_authentication_form": user_authentication_form,
+        "events_types": events_types,
+    })
+
+
+def view_event_type(request, pk):
+    event_type = EventsType.objects.get(pk=pk)
+    events = Events.objects.filter(event_type_id=event_type.id)
+    user_form = UserCreationForm()
+    user_authentication_form = AuthenticationForm()
+    events_types = EventsType.objects.all()
+    return render(request, 'events_types/events_type.html', {
+        "event_type": event_type,
+        "events": events,
+        "user_creation_form": user_form,
+        "user_authentication_form": user_authentication_form,
+        "events_types": events_types,
+    })
