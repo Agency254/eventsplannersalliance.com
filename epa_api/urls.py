5386ccdfc0f88e1e4007422c1dc855d0a76c2d30
newtonkiragu <karanunewton4@gmail.com>
2020-03-18 01:21:17 +0300

    registered profiles and users views

--- epa_api/urls.py
+++ epa_api/urls.py
@@ -5,12 +5,13 @@ from rest_framework import routers
 from epa_api import views
 
 # Routers provide an easy way of automatically determining the URL conf.
-from epa_api.views import UserViewSet, EventsListView, MerchantsListView, EventsTypeListView
+from epa_api.views import EventsListView, MerchantsListView, EventsTypeListView, ProfileListView, UserListView
 
 router = routers.DefaultRouter()
 router.register(r'events', EventsListView)
 router.register(r'merchants', MerchantsListView)
-router.register(r'users', UserViewSet)
+router.register(r'profiles', ProfileListView)
+router.register(r'users', UserListView)
 router.register(r'events-type', EventsTypeListView)
 
 urlpatterns = [
