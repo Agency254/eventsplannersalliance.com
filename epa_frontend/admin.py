5ffc91915d9f8b95840def4d5cb9acde3174e8bd
newtonkiragu <karanunewton4@gmail.com>
2020-03-21 07:30:52 +0300

    removed orders and order items

--- epa_frontend/admin.py
+++ epa_frontend/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from epa_frontend.models import Merchants, Events, Profile, Orders, OrderItems, EventsType
+from epa_frontend.models import Merchants, Events, Profile, EventsType
 
 admin.site.register(Merchants)
 admin.site.register(Events)
 admin.site.register(Profile)
-admin.site.register(Orders)
-admin.site.register(OrderItems)
 admin.site.register(EventsType)
