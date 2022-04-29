0b2dc06d365c110b37b12979ed4d264fcc3e788d
Karanu Newton <karanunewton4@gmail.com>
2022-04-29 15:49:24 +0300

    setup django messages

--- epa_core/settings.py
+++ epa_core/settings.py
@@ -3,6 +3,16 @@ from decouple import config, Csv
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
